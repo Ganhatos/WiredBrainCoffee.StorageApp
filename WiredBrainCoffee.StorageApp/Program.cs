cca9c5b0a1fb684c1ba4d45d9c199fd6af3536a7
Ganhatos <ganhatos@gmail.com>
2021-07-14 11:30:11 -0300

    Add project files.

---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using WiredBrainCoffee.StorageApp.Data;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new SqlRepository<Employee>(new StorageAppDbContext());
+            AddEmployees(employeeRepository);
+            GetEmployeeById(employeeRepository);
+
+            var organizationRepository = new ListRepository<Organization>();
+            AddOrganizations(organizationRepository);
+        }
+
+        private static void GetEmployeeById(IRepository<Employee> employeeRepository)
+        {
+            var employee = employeeRepository.GetById(2);
+            Console.WriteLine($"Employee with Id 2: {employee.FirstName}");
+        }
+
+        private static void AddEmployees(IRepository<Employee> employeeRepository)
+        {
+            employeeRepository.Add(new Employee { FirstName = "Julia" });
+            employeeRepository.Add(new Employee { FirstName = "Anna" });
+            employeeRepository.Add(new Employee { FirstName = "Thomas" });
+            employeeRepository.Save();
+        }
+
+        private static void AddOrganizations(ListRepository<Organization> organizationRepository)
+        {
+            organizationRepository.Add(new Organization { Name = "Pluralsight" });
+            organizationRepository.Add(new Organization { Name = "Globomantics" });
+            organizationRepository.Save();
+        }
+    }
+}
